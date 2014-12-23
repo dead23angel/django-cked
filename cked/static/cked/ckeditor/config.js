9fb4d8b554dbd35cd6f0491775545c9e558da4fd
Hemanth <hemanth7787@gmail.com>
2014-12-23 15:03:03 +0530

    Ckeditor downgraded

--- cked/static/cked/ckeditor/config.js
+++ cked/static/cked/ckeditor/config.js
@@ -4,7 +4,6 @@
  */
 
 CKEDITOR.editorConfig = function( config ) {
-	config.filebrowserBrowseUrl = '';
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
